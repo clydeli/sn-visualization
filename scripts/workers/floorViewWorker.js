a2b76779afde0de2b58772b76ee7baab2669ef04
clydeli <clyde1008li@gmail.com>
2013-04-28 14:54:32 -0700

    fix css invalid syntax, add color to represent health status

---
+++ scripts/workers/floorViewWorker.js
@@ -0,0 +1,30 @@
+var
+  url = "",
+  interval = 9000;
+
+function pollData(){
+  try {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200 || xhr.status ==0) { postMessage(xhr.responseText); }
+        else { throw  xhr.status+xhr.responseText; }
+        setTimeout( function(){ pollData(); }, interval);
+      }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+  } catch(e){ postMessage("ERROR:"+e.message);}
+}
+
+self.addEventListener('message', function(e) {
+  switch(e.data.type){
+    case "START":
+      url = e.data.url;
+      pollData();
+      break;
+    case "STOP":
+      self.close();
+      break;
+  }
+}, false);
